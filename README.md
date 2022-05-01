a2f0bc8ca367d07699d5f3271b905e9929044b03
lliilliiili <104695211+lliilliiili@users.noreply.github.com>
2022-05-01 17:57:49 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# echoBotExample
+### 請先將下方按鈕連結github帳號名稱與repository名稱改成自己的
+<a href="https://heroku.com/deploy?template=https://github.com/githublliilliiili/repositoryAAAAA/blob/main">
+  <img src="https://www.herokucdn.com/deploy/button.svg" alt="Deploy">
+</a>
---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, request, abort
+import os
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+
+from linebot.models import *
+
+
+app = Flask(__name__)
+
+
+line_bot_api = LineBotApi(os.environ.get("CHANNEL_ACCESS_TOKEN"))
+handler = WebhookHandler(os.environ.get("CHANNEL_SECRET"))
+
+@app.route("/", methods=['GET'])
+def hello():
+    return "Hello World!"
+
+@app.route("/", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    print("Request body: " + body, "Signature: " + signature)
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+       abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    msg = event.message.text
+    print(msg)
+    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=f"{msg}"))
